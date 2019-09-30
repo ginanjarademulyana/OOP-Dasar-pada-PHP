b8ec37bb66ef7aac36f6ddf67bb412a7f90b932a
Ginanjar Ade Mulyana <dorion.official@gmail.com>
2019-09-30 18:16:02 +0700

    menambahkan tipe data jmlHalaman, waktuMain, dan tipe

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -4,17 +4,33 @@ class Produk {
     public  $judul,
             $penulis,
             $penerbit,
-            $harga;
+            $harga,
+            $jmlHalaman,
+            $waktuMain,
+            $tipe;
 
-            public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0){
+            public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman, $waktuMain, $tipe){
                 $this->judul = $judul;
                 $this->penulis = $penulis;
                 $this->penerbit = $penerbit;
-                $this->harga = $harga;
+                $this->harga = $harga,
+                $this->jmlHalaman = $jmlHalaman,
+                $this->waktuMain = $waktuMain,
+                $this->tipe = $tipe;
             }
 
     public function getLabel(){
         return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoLengkap(){
+        $str = "{$this->tipe} : {$this->getLabel()} (Rp. {$this->harga})";
+
+        if( $this->tipe == "Komik"){
+            $str .= " - {$this->jmlHalaman} Halaman.";
+        }else if ($this->tipe == "Game"){
+            $str .= " ~ {$this->waktuMain} Jam.";
+        }
     }
 
 }
@@ -26,15 +42,10 @@ class CetakInfoProduk {
     }
 }
 
-$produk1 = new Produk("Naruto","Mashasi Koshimoto", "Shonen Jump", 30000);
-$produk2 = new Produk("Uncharted","Neil Druckman", "Sony Computer", 25000);
-$produk3 = new Produk("Dragon Ball");
+$produk1 = new Produk("Naruto","Mashasi Koshimoto", "Shonen Jump", 30000, 100, null, "Komik");
+$produk2 = new Produk("Uncharted","Neil Druckman", "Sony Computer", 25000, null, 50, "Game");
 
-echo "Komik : " . $produk1->getLabel();
-echo "<br>";
-echo "Game : " . $produk2->getLabel();
+// Komik : Mashasi Koshimoto, Shonen Jump
+// Game : Neil Druckman, Sony Computer
+// Naruto | Mashasi Koshimoto, Shonen Jump, (Rp. 30000)
 
-echo "<br>";
-
-$infoProduk1 = new CetakInfoProduk();
-echo $infoProduk1->cetak($produk1);
