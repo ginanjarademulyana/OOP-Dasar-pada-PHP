febbe554e8837ed7030d19d6eb56d25a47c16057
Ginanjar Ade Mulyana <dorion.official@gmail.com>
2019-09-30 16:44:16 +0700

    Menambahkan produk3 dengan inisialisasi lengkap

--- produk.php
+++ produk.php
@@ -1,9 +1,25 @@
 <?php 
 
 class Produk {
-    public $judul, $penulis, $penerbit, $harga;
+    public  $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
 
 }
 
-$produk1 = new Produk();
- var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "Hahahahah";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Dorion";
+$produk3->harga = 30000;
+var_dump($produk3);
